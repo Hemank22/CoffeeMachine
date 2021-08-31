163f092e9e781e6c3c42bb3867dd49133864c3dc
Hemank Mehtani <85116042+Hemank22@users.noreply.github.com>
2021-08-31 20:12:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+#print report
+#check resources sufficiency
+#coin operated
+#check if transaction is successful
+#make coffee
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
